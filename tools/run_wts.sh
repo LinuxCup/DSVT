38133cfcd058434fac1500434ec65954dd2793bd
郑虎 <zhenghu@zhito.com>
2023-12-13 14:52:43 +0800

    add wts scripts for train branch
    Change-Id: I3e9af289c1af50d5cca2836e65dc6c8b80fd6ee3

---
+++ tools/run_wts.sh
@@ -0,0 +1,12 @@
+python gen_wts_test.py --cfg_file cfgs/dsvt_models/dsvt_plain_1f_onestage_zhito_5w_150epoch_lr002_range_adam_300epoch_2dsvt_1_2rpn.yaml \
+--ckpt /home/zhenghu/DeepLearning/DSVT_train/DSVT/output/dsvt_models/dsvt_plain_1f_onestage_zhito_5w_150epoch_lr002_range_adam_300epoch_2dsvt_1_2rpn/default/ckpt/latest_model.pth \
+--data_path /home/zhenghu/datasets/zhito/J6 \
+--ext .bin
+
+
+# python gen_wts_test.py --cfg_file cfgs/dsvt_models/dsvt_plain_1f_onestage.yaml \
+# --ckpt /home/zhenghu/DeepLearning/DSVT/output/cfgs/dsvt_models/dsvt_plain_1f_onestage/default/ckpt/latest_model.pth \
+# --data_path /home/zhenghu/datasets/zhito/J6 \
+# --ext .bin
+
+# python demo.py --cfg_file cfgs/dsvt_models/dsvt_plain_1f_onestage_trtengine.yaml --ckpt /home/zhenghu/DeepLearning/DSVT/output/cfgs/dsvt_models/dsvt_plain_1f_onestage/default/ckpt/latest_model.pth --data_path /home/zhenghu/datasets/waymo/waymo_format/waymo/test
